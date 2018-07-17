0f633a62884969b5b8319fd8386f9dbed1bc94b8
Danling Wu <k1630522@kcl.ac.uk>
2018-07-17 20:43:25 +0100

    tidy up and move round code to a new js file

---
+++ js/round.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  /** round button code **/
+     /*  	var timeout_id = 0,
+      hold_time = 1000,
+      hold_trigger = $('.hold_trigger');
+      var count = 5;
+      hold_trigger.mousedown(function() {
+        function dosetTimeout(count){
+          setTimeout(2000);
+
+          console.log(count);
+        }
+          for(count=5; count>=0; count--){
+            dosetTimeout(count);
+            // Something you want delayed.
+            $("#check-in-btn").text("holding..."+count).button("refresh");
+            console.log($("#check-in-btn").text());
+          }
+
+      }).bind('mouseup mouseleave', function() {
+          $("#check-in-btn").text("Check in").button("refresh");
+        count = 5;
+      });
+
+  function menu_toggle() {
+      $('.spinner').addClass('active');
+  }*/
+});
